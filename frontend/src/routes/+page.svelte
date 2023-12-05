174956aa13819273b21cb8291182720cd827600e
Felipe Escárate <felipeescaratefernandez@gmail.com>
2023-12-05 00:33:29 -0300

    refactor: move navbar to layout.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,4 @@
-<script>
-    import Navbar from './Navbar.svelte';
-</script>
-
 <main>
-    <Navbar />
     <div class='p-4 mx-10 my-5 rounded-xl border-4 border-blue-300'>
         <h1 class='text-4xl font-bold'>Page</h1>
     </div>
--- frontend/src/routes/add/+page.svelte
+++ frontend/src/routes/add/+page.svelte
@@ -1,10 +1,8 @@
 <script>
-    import Navbar from '../Navbar.svelte';
     import Form from './Form.svelte';
 </script>
 
 <main>
-    <Navbar />
     <div class='mx-10 my-5 rounded-xl border-4 border-blue-300 h-full p-4 space-y-4'>
         <h1 class='text-4xl font-bold'>Add Entry</h1>
         <Form />
