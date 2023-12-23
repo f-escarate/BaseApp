44874664217a298bda74d021a35ad21ccfcff40b
Felipe Escárate <felipe.escarate@ug.uchile.cl>
2023-12-22 23:34:41 -0300

    feat: add tailwind-animated plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,10 @@
 const config = {
   content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin'),
+    require('tailwindcss-animated')
+  ],
 
   darkMode: 'class',
 
