eec72066b2c2b2b4b9cb43539f1ef944547c85d4
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-01-02 22:39:15 -0300

    change: add page styles

--- frontend/src/routes/add/+page.svelte
+++ frontend/src/routes/add/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-    <div class='mx-10 my-5 rounded-xl border-4 border-blue-300 h-full p-4 space-y-4'>
+    <div class='mx-auto w-[75%] my-5 h-full p-4 space-y-4'>
         <h1 class='text-4xl font-bold'>Add Entry</h1>
         <Form />
     </div>
--- frontend/src/routes/add/Form.svelte
+++ frontend/src/routes/add/Form.svelte
@@ -23,28 +23,26 @@
     }
 
 </script>
-<main>
-    <div class='rounded-xl border-4 border-blue-300 p-5'>
-        <div class='grid gap-6 items-end w-full md:grid-cols-2'>
-            <div class='md:col-span-2'>
-                <Label for="title" class="mb-2">Title</Label>
-                <FloatingLabelInput style="filled" name="title" id="title" type="text" label="Title" bind:value={title}/>
-            </div>
-            <div class='md:col-span-1 h-full'>
-                <Label for="continuous-var">Continuous variable</Label>
-                <Range id="continuous-var" name="continuous-var" min="0" max="10" step="0.001" bind:value={continuousVar}/>
-                <p class='text-xs'>Value: {continuousVar}</p>
-
-                <Label for="discrete-var">Discrete variable</Label>
-                <Range id="discrete-var" name="discrete-var" min="0" max="10" step="1" bind:value={discreteVar}/>
-                <p class='text-xs'>Value: {discreteVar}</p>
-            </div>
-
-            <div>
-                <Label for="description" class="mb-2">Description</Label>
-                <Textarea id="description" placeholder="Enter description" rows="5" name="description" bind:value={description}/>
-            </div>
-            <Button class='md:col-span-2 w-[50%] mx-auto' color="green" on:click={handleSubmit} pill>Add</Button>
+<main class='border-4 border-blue-300 p-5'>
+    <div class='grid gap-6 items-end w-full md:grid-cols-2'>
+        <div class='md:col-span-2'>
+            <Label for="title" class="mb-2">Title</Label>
+            <FloatingLabelInput style="filled" name="title" id="title" type="text" label="Title" bind:value={title}/>
         </div>
+        <div class='md:col-span-1 h-full'>
+            <Label for="continuous-var">Continuous variable</Label>
+            <Range id="continuous-var" name="continuous-var" min="0" max="10" step="0.001" bind:value={continuousVar}/>
+            <p class='text-xs'>Value: {continuousVar}</p>
+
+            <Label for="discrete-var">Discrete variable</Label>
+            <Range id="discrete-var" name="discrete-var" min="0" max="10" step="1" bind:value={discreteVar}/>
+            <p class='text-xs'>Value: {discreteVar}</p>
+        </div>
+
+        <div>
+            <Label for="description" class="mb-2">Description</Label>
+            <Textarea id="description" placeholder="Enter description" rows="5" name="description" bind:value={description}/>
+        </div>
+        <Button class='md:col-span-2 w-[50%] mx-auto' color="green" on:click={handleSubmit} pill>Add</Button>
     </div>
 </main>
