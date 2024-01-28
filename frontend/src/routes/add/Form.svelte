89b8f134c9c65d1fdbea2629f2fec559c316d663
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-01-28 17:01:58 -0300

    feat: add image input to form

--- frontend/src/routes/add/Form.svelte
+++ frontend/src/routes/add/Form.svelte
@@ -22,6 +22,14 @@
         console.log(json);
     }
 
+    const loadFile = (event) => {
+        var output = document.getElementById('preview');
+        output.src = URL.createObjectURL(event.target.files[0]);
+        output.onload = () => {
+            URL.revokeObjectURL(output.src) // free memory
+        }
+    };
+
 </script>
 <main class='border-4 border-blue-300 p-5'>
     <div class='grid gap-6 items-end w-full md:grid-cols-2'>
@@ -30,16 +38,16 @@
             <FloatingLabelInput style="filled" name="title" id="title" type="text" label="Title" bind:value={title}/>
         </div>
         <div class='md:col-span-1 h-full flex flex-col justify-between'>
-            <div>
+            <div class="mb-4">
                 <Label for="continuous-var">Continuous variable</Label>
                 <Range id="continuous-var" name="continuous-var" min="0" max="10" step="0.001" bind:value={continuousVar}/>
-                <p class='text-xs'>Value: {continuousVar}</p>
+                <p class='text-xs italic'>Value: {continuousVar}</p>
             </div>
 
-            <div>
+            <div class="mb-4">
                 <Label for="discrete-var">Discrete variable</Label>
                 <Range id="discrete-var" name="discrete-var" min="0" max="10" step="1" bind:value={discreteVar}/>
-                <p class='text-xs'>Value: {discreteVar}</p>
+                <p class='text-xs italic'>Value: {discreteVar}</p>
             </div>
 
             <div class="flex flex-col">
@@ -53,6 +61,15 @@
             <Textarea class="h-full" id="description" placeholder="Enter description" rows="5" name="description" bind:value={description}/>
         </div>
 
+        <div class="my-2 flex-row align-center">
+            <Label for="image" class="mb-2">
+            Upload image
+            <div class="my-4 p-1 flex bg-blue-300 hover:bg-blue-500 text-center hover:text-white transition-colors duration-400 ease-in-out">
+                <img id="preview" class="md:w-1/2 m-auto p-2" alt="Data">
+                <input type="file" id="image" name="image" class="w-0 invisible" on:change={loadFile}>
+            </div>
+            </Label>
+        </div>
         <Button class='md:col-span-2 w-[50%] mx-auto h-full' color="green" on:click={handleSubmit} pill>Add</Button>
     </div>
 </main>
