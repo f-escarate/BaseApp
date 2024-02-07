f74665c7e7c17e97476174ad895b8525f9b8614d
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-02-07 00:16:42 -0300

    feat: add 'User button'

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -4,6 +4,7 @@
     var activeClass = nonActiveClass+' md:border-blue-400 md:border-b-2 md:rounded-none'
 
     import { page } from '$app/stores';
+    import UserButton from './UserButton.svelte';
     $: activeUrl = $page.url.pathname;
 </script>
 
@@ -12,7 +13,7 @@
         <img src='/favicon.png' class="me-3 h-6 sm:h-9" alt="Logo" />
         <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">Base App</span>
     </NavBrand>
-    <NavHamburger/>
+    <UserButton/>
     <NavUl {activeUrl} {activeClass} {nonActiveClass}>
         <NavLi href="/">Home</NavLi>
         <NavLi href="/add/">Add Entry</NavLi>
---
+++ frontend/src/components/UserButton.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { Avatar, Dropdown, DropdownHeader, DropdownItem, DropdownDivider, NavHamburger, Button } from 'flowbite-svelte';
+    let name = 'John Doe';
+    let email = 'john@doe.com';
+    let autenticated = true;
+    let image = '';
+</script>
+
+{#if autenticated}
+    <div class="flex items-center md:order-2">
+        <Avatar id="avatar-menu" src={image} />
+        <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+    </div>
+    <Dropdown placement="bottom" triggeredBy="#avatar-menu">
+        <DropdownHeader>
+            <span class="block text-sm">{name}</span>
+            <span class="block truncate text-sm font-medium">{email}</span>
+        </DropdownHeader>
+        <DropdownItem>My Entries</DropdownItem>
+        <DropdownItem>Account Settings</DropdownItem>
+        <DropdownDivider />
+        <DropdownItem>Sign out</DropdownItem>
+    </Dropdown>
+{:else}
+    <div class="flex md:order-2">
+        <Button size="sm">Login</Button>
+    </div>
+{/if}
