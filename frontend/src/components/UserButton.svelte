f3bb9e40ff124b8c65422a2954ac742d6b005090
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-02-07 23:18:53 -0300

    feat: add log in, log out and sign up to frontend

--- frontend/src/components/UserButton.svelte
+++ frontend/src/components/UserButton.svelte
@@ -1,28 +1,60 @@
 <script>
     import { Avatar, Dropdown, DropdownHeader, DropdownItem, DropdownDivider, NavHamburger, Button } from 'flowbite-svelte';
-    let name = 'John Doe';
-    let email = 'john@doe.com';
-    let autenticated = true;
-    let image = '';
+    import UserIcon from './icons/UserIcon.svelte';
+    import Cookies from 'js-cookie';
+    import { onMount } from 'svelte';
+    const HOST = 'http://localhost:8000';
+    let data = {
+        name: '',
+        email: '',
+        image: '',
+        authenticated: false
+    };
+    onMount(async () => {
+        const response = 
+            await fetch(`${HOST}/me/`, {
+                method: "GET",
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${Cookies.get('token')}`
+                }
+            });
+        const json = await response.json();
+        if (json.status == 'success') {
+            data.name = json.username;
+            data.email = json.email;
+            data.image = '';
+            data.authenticated=true;
+        }
+    });
+    const handleLogOut = () => {
+        Cookies.remove('token');
+        data.authenticated = false;
+        window.location.href = '/';
+    }
+
 </script>
 
-{#if autenticated}
+{#if data.authenticated}
     <div class="flex items-center md:order-2">
-        <Avatar id="avatar-menu" src={image} />
+        <Avatar id="avatar-menu" src={data.image} />
         <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
     </div>
     <Dropdown placement="bottom" triggeredBy="#avatar-menu">
         <DropdownHeader>
-            <span class="block text-sm">{name}</span>
-            <span class="block truncate text-sm font-medium">{email}</span>
+            <span class="block text-sm">{data.name}</span>
+            <span class="block truncate text-sm font-medium">{data.email}</span>
         </DropdownHeader>
         <DropdownItem>My Entries</DropdownItem>
         <DropdownItem>Account Settings</DropdownItem>
         <DropdownDivider />
-        <DropdownItem>Sign out</DropdownItem>
+        <DropdownItem on:click={handleLogOut}>Log out</DropdownItem>
     </Dropdown>
 {:else}
-    <div class="flex md:order-2">
-        <Button size="sm">Login</Button>
+    <div class="flex md:order-2 gap-2">
+        <Button size="sm" href="/login">
+            <UserIcon sx='m-0 text-white'/>
+            Log In
+        </Button>
     </div>
 {/if}
---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+    import { Tabs, TabItem, FloatingLabelInput , Button} from 'flowbite-svelte';
+    import Cookies from 'js-cookie';
+    const HOST = 'http://localhost:8000';
+    let name = '';
+    let email = '';
+    let pass = '';
+    let pass2 = '';
+
+    const handleLogin = async () => {
+        const data = new FormData();
+        data.append('username', email);
+        data.append('password', pass);
+        const response = await fetch('http://localhost:8000/token', {
+            method: "POST",
+            body: data
+        });
+        const json = await response.json();
+        console.log(json);
+        if (json.status == 'success') {
+            alert('Successfully logged in');
+            Cookies.set('token', json.access_token, { sameSite:'strict' });
+            window.location.href = '/';
+        }
+    }
+
+    const handleRegister = async () => {
+        if (pass != pass2) {
+            alert('Passwords do not match');
+            return;
+        }
+        const data = new FormData();
+        data.append('username', name);
+        data.append('email', email);
+        data.append('password', pass);
+        data.append('password2', pass2);
+        console.log(data);
+        const response = await fetch(`${HOST}/register/`, {
+            method: "POST",
+            body: data
+        });
+        const json = await response.json();
+        console.log(json);
+        if (json.status == 'success') {
+            alert('Successfully registered');
+            window.location.href = '/login';
+        }
+    }
+
+
+</script>
+
+<div class='mx-auto max-w-[50%] my-5 h-full p-4 space-y-4'>
+    <Tabs style="underline">
+        <TabItem open>
+            <span slot="title">Log In</span>
+            <div class='flex flex-col gap-4'>
+                <FloatingLabelInput style="filled" name="email" id="email" type="text" label="Email" bind:value={email}/>
+                <FloatingLabelInput style="filled" name="password" id="password" type="password" label="Password" bind:value={pass}/>
+                <Button size="sm" on:click={handleLogin}>Log In</Button>
+            </div>
+        </TabItem>
+        <TabItem>
+            <span slot="title">Register</span>
+            <div class='flex flex-col gap-4'>
+                <FloatingLabelInput style="filled" name="name" id="name" type="text" label="Name" bind:value={name}/>
+                <FloatingLabelInput style="filled" name="email" id="register_email" type="text" label="Email" bind:value={email}/>
+                <FloatingLabelInput style="filled" name="password" id="register_password" type="password" label="Password" bind:value={pass}/>
+                <FloatingLabelInput style="filled" name="password2" id="register_password2" type="password" label="Repeat your password" bind:value={pass2}/>
+                <Button size="sm" on:click={handleRegister}>Register</Button>
+            </div>
+        </TabItem>
+    </Tabs>    
+</div>
+  
