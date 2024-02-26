484233dff0c3d7907f72050c76e095937be0b112
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-02-25 23:53:26 -0300

    feat: add footer

---
+++ frontend/src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Footer, FooterCopyright, FooterBrand } from 'flowbite-svelte';
+</script>
+  
+<Footer footerType="logo">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <FooterBrand href="/" src="/favicon.png" alt="Base App Logo" name="Base App" />
+    </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <FooterCopyright href="/" by="Base App" />
+</Footer>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
     import "../app.css";
+    import Footer from "../components/Footer.svelte";
     import Navbar from "../components/Navbar.svelte";
 </script>
-<Navbar/>
 
+<Navbar/>
 <div class='mx-auto w-[75%] my-5 h-full p-4 space-y-4'>
     <slot />
 </div>  
+<Footer/>
