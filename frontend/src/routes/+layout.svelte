2e8e26b255631bea1fb55c6bfe47d6cd696291f5
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-02-26 00:22:49 -0300

    fix: footer always on the bottom of the screen

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,8 +4,10 @@
     import Navbar from "../components/Navbar.svelte";
 </script>
 
-<Navbar/>
-<div class='mx-auto w-[75%] my-5 h-full p-4 space-y-4'>
-    <slot />
-</div>  
-<Footer/>
+<div class='min-h-screen flex flex-col justify-between'>
+    <Navbar/>
+    <div class='mx-auto w-[75%] my-5 h-full p-4 space-y-4'>
+        <slot />
+    </div>  
+    <Footer/>
+</div>
